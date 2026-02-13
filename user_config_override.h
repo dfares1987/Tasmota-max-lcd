fe85f060389f74b4cbabf9a4e0dcdb102a563032
dfares1987 <dfares@gmail.com>
2026-02-13 13:33:00 -0300

    Add user_config_override.h with configuration guards

---
+++ user_config_override.h
@@ -0,0 +1,15 @@
+#ifndef USE_I2C
+#define USE_I2C
+#endif
+
+#ifndef USE_DISPLAY
+#define USE_DISPLAY
+#endif
+
+#ifndef USE_DISPLAY_LCD
+#define USE_DISPLAY_LCD
+#endif
+
+#ifndef USE_MAX31855
+#define USE_MAX31855
+#endif
